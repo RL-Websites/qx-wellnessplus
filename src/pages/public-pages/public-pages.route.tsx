d615b98dfe5ebbcd63c7cc878470e3fd2905cd95
obaedul islam <obaedul@redlimesolutions.com>
2025-08-04 15:16:50 +0600

    Registration Page development done

--- src/pages/public-pages/public-pages.route.tsx
+++ src/pages/public-pages/public-pages.route.tsx
@@ -1,6 +1,7 @@
 import { Navigate, RouteObject } from "react-router-dom";
 import CategoryPage from "./category/category.page";
 import HomePage from "./home/home.page";
+import RegistrationPage from "./registration/registration.page";
 
 export const PublicPagesRoute: RouteObject[] = [
   {
@@ -14,5 +15,9 @@ export const PublicPagesRoute: RouteObject[] = [
   {
     path: "category",
     element: <CategoryPage />,
+  },
+  {
+    path: "registration",
+    element: <RegistrationPage />,
   },
 ];
