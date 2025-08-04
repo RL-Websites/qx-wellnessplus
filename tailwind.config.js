8e346a37876f93307edee752871b529da203e961
yeasinRL <57594853+yeasin2448@users.noreply.github.com>
2025-08-04 10:52:18 +0600

    category component update

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,6 +72,15 @@ export default {
         xl: "1280px",
         "2xl": "1536px",
       },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
     },
   },
   plugins: [],
