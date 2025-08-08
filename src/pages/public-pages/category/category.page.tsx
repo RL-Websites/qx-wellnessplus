3449b83c54fb1caa2f5a9b4640226f9eaefb6b5d
obaedul islam <obaedul@redlimesolutions.com>
2025-08-08 10:56:00 +0600

    Category Image tunning & link setup done

--- src/pages/public-pages/category/category.page.tsx
+++ src/pages/public-pages/category/category.page.tsx
@@ -6,12 +6,17 @@ const CategoryPage = () => {
     <div className="space-y-12">
       <h4 className="lg:text-[90px] md:text-6xl sm:text-4xl text-2xl text-center text-foreground uppercase">Choose A Treatment</h4>
 
-      <div className={`${categoryCard.length < 3 ? `flex flex-wrap justify-center` : "grid lg:grid-cols-3 sm:grid-cols-2"} lg:gap-20 md:gap-10 gap-5`}>
+      <div
+        className={`${
+          categoryCard.length < 3 ? `flex flex-wrap justify-center` : "grid lg:grid-cols-3 sm:grid-cols-2"
+        } lg:gap-y-12 md:gap-y-10 gap-y-5 lg:gap-x-20 md:gap-x-10 gap-x-5`}
+      >
         {categoryCard?.map((item, index) => (
           <CategoryCard
             key={index}
             image={item?.image}
             title={item?.title}
+            link="quiz"
           />
         ))}
       </div>
