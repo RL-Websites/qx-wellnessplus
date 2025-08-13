7c81d5e941727845535562c26dc3389282f36aa3
appl4e <appl4e@gmail.com>
2025-08-13 10:06:55 +0600

    home page header

--- src/common/layouts/HomeLayout.tsx
+++ src/common/layouts/HomeLayout.tsx
@@ -1,9 +1,12 @@
+import HomeHeader from "./Components/HomeHeader";
+
 interface homeLayoutProps {
   children: React.ReactNode;
 }
 const HomeLayout = ({ children }: homeLayoutProps) => {
   return (
     <div className="site-main-bg  lg:pt-16 pt-10 lg:pb-24 pb-10">
+      <HomeHeader />
       <div className="container mx-auto">
         <div className="">{children}</div>
       </div>
